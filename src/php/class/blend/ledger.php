94fb835619af2f8b47f27e436d05b470f383d9c6
Oran Fry <oran@emerg.co>
2020-04-13 12:08:27 +1200

    Ledger Package

---
+++ src/php/class/blend/ledger.php
@@ -0,0 +1,52 @@
+<?php
+namespace blend;
+
+class ledger extends \Blend
+{
+    public function __construct()
+    {
+        $this->label = 'Ledger';
+        $this->linetypes = ['transaction'];
+        $this->groupby = 'date';
+        $this->fields = [
+            (object) [
+                'name' => 'icon',
+                'type' => 'icon',
+                'derived' => true,
+            ],
+            (object) [
+                'name' => 'date',
+                'type' => 'date',
+                'groupable' => true,
+                'main' => true,
+            ],
+            (object) [
+                'name' => 'account',
+                'type' => 'text',
+                'groupable' => true,
+            ],
+            (object) [
+                'name' => 'description',
+                'type' => 'text',
+                'default' => '',
+                'sacrifice' => true,
+            ],
+            (object) [
+                'name' => 'parenttype',
+                'type' => 'icon',
+                'derived' => true,
+                'translate' => [],
+                'customlink' => '"/line?type={$parenttype}&id={$parentid}&back=' . base64_encode($_SERVER['REQUEST_URI']) . '"',
+            ],
+            (object) [
+                'name' => 'amount',
+                'type' => 'number',
+                'summary' => 'sum',
+                'dp' => 2,
+            ],
+        ];
+        $this->past = true;
+        $this->cum = true;
+        $this->showass = ['list', 'calendar', 'graph', 'summaries'];
+    }
+}
---
+++ src/php/class/linetype/plaintransaction.php
@@ -0,0 +1,76 @@
+<?php
+namespace linetype;
+
+class plaintransaction extends \Linetype
+{
+    public function __construct()
+    {
+        $this->label = 'Transaction';
+        $this->icon = 'dollar';
+        $this->table = 'transaction';
+        $this->fields = null;
+        $this->summaries = null;
+        $this->showass = ['list', 'calendar', 'graph'];
+        $this->fields = [
+            (object) [
+                'name' => 'icon',
+                'type' => 'icon',
+                'fuse' => "'dollar'",
+                'derived' => true,
+            ],
+            (object) [
+                'name' => 'date',
+                'type' => 'date',
+                'groupable' => true,
+                'fuse' => 't.date',
+            ],
+            (object) [
+                'name' => 'account',
+                'type' => 'text',
+                'fuse' => 't.account',
+            ],
+            (object) [
+                'name' => 'description',
+                'type' => 'text',
+                'fuse' => 't.description',
+            ],
+            (object) [
+                'name' => 'amount',
+                'type' => 'number',
+                'dp' => 2,
+                'summary' => 'sum',
+                'fuse' => 't.amount',
+            ],
+        ];
+        $this->unfuse_fields = [
+            't.date' => ':date',
+            't.amount' => ':amount',
+            't.account' => ':account',
+            't.description' => ':description',
+        ];
+    }
+
+    public function get_suggested_values()
+    {
+        $suggested_values = [];
+
+        $suggested_values['account'] = get_values('transaction', 'account');
+
+        return $suggested_values;
+    }
+
+    public function validate($line)
+    {
+        $errors = [];
+
+        if ($line->date == null) {
+            $errors[] = 'no date';
+        }
+
+        if ($line->amount == null) {
+            $errors[] = 'no amount';
+        }
+
+        return $errors;
+    }
+}
