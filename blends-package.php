0185c2e96e20c57094462d6028474589d2bea0c9
Oran Fry <oran@emerg.co>
2020-04-13 13:04:35 +1200

    Add spending blend

--- blends-package.php
+++ blends-package.php
@@ -2,5 +2,6 @@
 return (object) [
     'blends' => [
         'ledger',
+        'spending',
     ],
 ];
---
+++ src/php/class/blend/spending.php
@@ -0,0 +1,33 @@
+<?php
+namespace blend;
+
+class spending extends \Blend
+{
+    public function __construct()
+    {
+        $this->label = 'Spending';
+        $this->amountltzero = true;
+        $this->past = false;
+        $this->cum = false;
+        $this->linetypes = ['transaction'];
+        $this->groupby = 'account';
+        $this->fields = [
+            (object) [
+                'name' => 'date',
+                'type' => 'date',
+                'main' => true,
+            ],
+            (object) [
+                'name' => 'account',
+                'type' => 'text',
+            ],
+            (object) [
+                'name' => 'amount',
+                'type' => 'number',
+                'summary' => 'sum',
+                'dp' => 2,
+            ],
+        ];
+        $this->showass = ['summaries', 'pie'];
+    }
+}
