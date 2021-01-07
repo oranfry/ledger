8b2b640d05ecd40e336f65d1e1ac6c9620505b4b
Oran Fry <oran@emerg.co>
2021-01-07 20:33:09 +1300

    Nav
    Don't make display of delete button conditional on using jars!

--- src/php/partial/nav/ledger/index.php
+++ src/php/partial/nav/ledger/index.php
@@ -4,11 +4,11 @@
 <?php if ($hasJars) : ?>
     <?php ContextVariableSet::get('jar')->display(); ?>
     <?php if ($hasSuperjars) : ?><?php ContextVariableSet::get('superjar')->display(); ?><?php endif ?>
-
-    <div class="navset">
-        <i class="icon icon--gray icon--times delete-selected"></i>
-    </div>
 <?php endif ?>
+
+<div class="navset">
+    <i class="icon icon--gray icon--times delete-selected"></i>
+</div>
 
 <?php $showas->display(); ?>
 <?php $repeater->display(); ?>
