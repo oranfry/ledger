e6d7aad29951d262c31d716f8f3bbf2496956dcd
Oran Fry <oran@emerg.co>
2023-04-09 21:17:11 +1200

    List View
    Restore expanding table to full screen on smaller screens.

--- src/php/partial/showas/ledger/list.php
+++ src/php/partial/showas/ledger/list.php
@@ -2,125 +2,123 @@
 <?php $daterange = ContextVariableSet::get('daterange'); ?>
 <?php $num_visible_cols = count($fields) - count($mask_fields); ?>
 <?php $seen_today = !@$currentgroup || strcmp($currentgroup, $daterange->from ?? '0000-00-00') < 0 || strcmp($currentgroup, $daterange->to ?? '9999-12-31') > 0; ?>
-<div style="float: left">
-    <table class="easy-table">
-        <thead>
-            <tr>
-                <th class="select-column printhide"><i class="icon icon--gray icon--smalldot-o selectall"></i></td></th>
-                <?php foreach ($fields as $field): ?>
-                    <th class="<?= $field->type == 'number' ? 'right' : '' ?>" <?php if (in_array($field->name, $mask_fields)): ?>style="display: none"<?php endif ?>><?= !@$field->supress_header && @$field->type != 'icon' ? $field->name : '' ?></th>
-                <?php endforeach ?>
-            </tr>
-        </thead>
-        <tbody>
-            <?php for ($i = 0; $i <= count($lines); $i++): ?>
-                <?php
-                    $skip = false;
-                    unset($line);
+<table class="easy-table">
+    <thead>
+        <tr>
+            <th class="select-column printhide"><i class="icon icon--gray icon--smalldot-o selectall"></i></td></th>
+            <?php foreach ($fields as $field): ?>
+                <th class="<?= $field->type == 'number' ? 'right' : '' ?>" <?php if (in_array($field->name, $mask_fields)): ?>style="display: none"<?php endif ?>><?= !@$field->supress_header && @$field->type != 'icon' ? $field->name : '' ?></th>
+            <?php endforeach ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php for ($i = 0; $i <= count($lines); $i++): ?>
+            <?php
+                $skip = false;
+                unset($line);
 
-                    if ($i == count($lines)) {
-                        if (!$seen_today) {
-                            $line = (object) ['date' => $currentgroup];
-                        } else {
-                            $line = (object) ['date' => null];
-                        }
-
-                        $skip = true;
+                if ($i == count($lines)) {
+                    if (!$seen_today) {
+                        $line = (object) ['date' => $currentgroup];
                     } else {
-                        $line = $lines[$i];
+                        $line = (object) ['date' => null];
+                    }
+
+                    $skip = true;
+                } else {
+                    $line = $lines[$i];
+                }
+            ?>
+
+            <?php if (@$summaries[@$lastgroup] && ($i == count($lines) || $line->date != $lastgroup)): ?>
+                <?php $summary = $summaries[$lastgroup]; ?>
+                <tr>
+                    <td class="select-column printhide"></td>
+                    <?php foreach ($fields as $field): ?>
+                        <td class="<?= $field->type == 'number' ? 'right' : '' ?>" <?php if (in_array($field->name, $mask_fields)): ?>style="display: none"<?php endif ?>>
+                            <?php if (@$summary->{$field->name}): ?>
+                                <strong><?= @$field->prefix . $summary->{$field->name} ?></strong>
+                            <?php endif ?>
+                        </td>
+                    <?php endforeach ?>
+                </tr>
+            <?php endif ?>
+
+            <?php if ($i == count($lines) || $line->date != $lastgroup): ?>
+                <?php
+                    if (!$seen_today && strcmp($currentgroup, @$line->date) < 0) {
+                        unset($line);
+
+                        $line = (object) ['date' => $currentgroup];
+                        $i--;
+                        $skip = true;
                     }
                 ?>
 
-                <?php if (@$summaries[@$lastgroup] && ($i == count($lines) || $line->date != $lastgroup)): ?>
-                    <?php $summary = $summaries[$lastgroup]; ?>
-                    <tr>
-                        <td class="select-column printhide"></td>
-                        <?php foreach ($fields as $field): ?>
-                            <td class="<?= $field->type == 'number' ? 'right' : '' ?>" <?php if (in_array($field->name, $mask_fields)): ?>style="display: none"<?php endif ?>>
-                                <?php if (@$summary->{$field->name}): ?>
-                                    <strong><?= @$field->prefix . $summary->{$field->name} ?></strong>
+                <?php if ($i > 0): ?>
+                    </tbody>
+                    <tbody>
+                <?php endif ?>
+
+                <?php if (@$line->date) : ?>
+                    <tr class="<?= strcmp($line->date, $currentgroup ?? '') ? '' : 'today' ?>">
+                        <td class="select-column printhide"><i class="icon icon--gray icon--smalldot-o selectall"></i></td>
+                        <?php $grouphref = strtok($_SERVER['REQUEST_URI'], '?') . '?' . ($daterange ? $daterange->constructQuery(['period' => 'day', 'rawrawfrom' => $line->date]) . '&' : '') . 'back=' . base64_encode($_SERVER['REQUEST_URI']); ?>
+                        <?php $grouptitle = "<a class=\"incog\" href=\"{$grouphref}\">" . $line->date . "</a>"; ?>
+                        <td colspan="<?= $num_visible_cols ?>" style="line-height: 2em; font-weight: bold">
+                            <?= $grouptitle ?>
+                            <div style="float: right" class="inline-rel">
+                                <?php if (count($addable) > 1): ?>
+                                    <div class="inline-modal inline-modal--right"><nav><?php foreach ($addable as $linetype): ?><?php if ($linetype instanceof \ledger\linetype\Transferout): ?><?php continue; ?><?php endif ?><a href="#" class="trigger-add-line" data-type="<?= $linetype->name ?>" data-date="<?= $line->date ?>"><i class="icon icon--gray icon--<?= $linetype instanceof \ledger\linetype\Transferin ? 'arrowleftright' : $linetype->icon ?>"></i></a><?php endforeach ?></nav></div>
+                                    <a class="inline-modal-trigger"><i class="icon icon--gray icon--plus"></i></a>
+                                <?php elseif (count($addable) == 1): ?>
+                                    <a href="#" class="trigger-add-line" data-type="<?= $addable[0]->name ?>" data-date="<?= $line->date ?>"><i class="icon icon--gray icon--plus"></i></a>
                                 <?php endif ?>
-                            </td>
-                        <?php endforeach ?>
+                            </div>
+                        </td>
                     </tr>
                 <?php endif ?>
+            <?php endif ?>
 
-                <?php if ($i == count($lines) || $line->date != $lastgroup): ?>
-                    <?php
-                        if (!$seen_today && strcmp($currentgroup, @$line->date) < 0) {
-                            unset($line);
+            <?php if (!@$skip): ?>
+                <tr
+                    <?= @$parent ? "data-parent=\"{$parent}\"" : '' ?>
+                    data-group="<?= $line->date ?>"
+                    class="linerow <?= @$line->broken ? 'broken' : null ?>"
+                    data-id="<?= $line->id ?>"
+                    data-type="<?= $line->type ?>"
+                >
+                    <td class="select-column printhide"><input type="checkbox"></td>
+                    <?php foreach ($fields as $field): ?>
+                        <?php $value = @$field->value ? computed_field_value($line, $field->value) : @$line->{$field->name}; ?>
+                        <td data-name="<?= $field->name ?>" data-value="<?= htmlspecialchars($value ?? '') ?>" style="<?php if ($field->type == 'number'): ?>text-align: right;<?php endif ?><?php if (in_array($field->name, $mask_fields)): ?>display: none;<?php endif ?>"><?php
+                            if ($field->type == 'icon') {
+                                ?><i class="icon icon--gray icon--<?= @$field->translate->{$value} ?? $value ?>"></i><?php
+                            } elseif ($field->type == 'color') {
+                                ?><span style="display: inline-block; height: 1em; width: 1em; background-color: #<?= $value ?>;">&nbsp;</span><?php
+                            } elseif ($field->type == 'file' && @$line->{"{$field->name}_path"}) {
+                               ?><a href="/api/download/<?= $line->{"{$field->name}_path"} ?>" download><i class="icon icon--gray icon--<?= @$field->translate[$field->icon] ?? $field->icon ?>"></i></a><?php
+                            } else {
+                                echo htmlspecialchars($value ?? '');
+                            }
+                        ?></td>
+                    <?php endforeach ?>
+                </tr>
+            <?php endif ?>
 
-                            $line = (object) ['date' => $currentgroup];
-                            $i--;
-                            $skip = true;
-                        }
-                    ?>
+            <?php $lastgroup = @$line->date; ?>
+            <?php $seen_today = $seen_today || ($lastgroup ?? '') == $currentgroup; ?>
+        <?php endfor ?>
+    </tbody>
+</table>
 
-                    <?php if ($i > 0): ?>
-                        </tbody>
-                        <tbody>
-                    <?php endif ?>
-
-                    <?php if (@$line->date) : ?>
-                        <tr class="<?= strcmp($line->date, $currentgroup ?? '') ? '' : 'today' ?>">
-                            <td class="select-column printhide"><i class="icon icon--gray icon--smalldot-o selectall"></i></td>
-                            <?php $grouphref = strtok($_SERVER['REQUEST_URI'], '?') . '?' . ($daterange ? $daterange->constructQuery(['period' => 'day', 'rawrawfrom' => $line->date]) . '&' : '') . 'back=' . base64_encode($_SERVER['REQUEST_URI']); ?>
-                            <?php $grouptitle = "<a class=\"incog\" href=\"{$grouphref}\">" . $line->date . "</a>"; ?>
-                            <td colspan="<?= $num_visible_cols ?>" style="line-height: 2em; font-weight: bold">
-                                <?= $grouptitle ?>
-                                <div style="float: right" class="inline-rel">
-                                    <?php if (count($addable) > 1): ?>
-                                        <div class="inline-modal inline-modal--right"><nav><?php foreach ($addable as $linetype): ?><?php if ($linetype instanceof \ledger\linetype\Transferout): ?><?php continue; ?><?php endif ?><a href="#" class="trigger-add-line" data-type="<?= $linetype->name ?>" data-date="<?= $line->date ?>"><i class="icon icon--gray icon--<?= $linetype instanceof \ledger\linetype\Transferin ? 'arrowleftright' : $linetype->icon ?>"></i></a><?php endforeach ?></nav></div>
-                                        <a class="inline-modal-trigger"><i class="icon icon--gray icon--plus"></i></a>
-                                    <?php elseif (count($addable) == 1): ?>
-                                        <a href="#" class="trigger-add-line" data-type="<?= $addable[0]->name ?>" data-date="<?= $line->date ?>"><i class="icon icon--gray icon--plus"></i></a>
-                                    <?php endif ?>
-                                </div>
-                            </td>
-                        </tr>
-                    <?php endif ?>
-                <?php endif ?>
-
-                <?php if (!@$skip): ?>
-                    <tr
-                        <?= @$parent ? "data-parent=\"{$parent}\"" : '' ?>
-                        data-group="<?= $line->date ?>"
-                        class="linerow <?= @$line->broken ? 'broken' : null ?>"
-                        data-id="<?= $line->id ?>"
-                        data-type="<?= $line->type ?>"
-                    >
-                        <td class="select-column printhide"><input type="checkbox"></td>
-                        <?php foreach ($fields as $field): ?>
-                            <?php $value = @$field->value ? computed_field_value($line, $field->value) : @$line->{$field->name}; ?>
-                            <td data-name="<?= $field->name ?>" data-value="<?= htmlspecialchars($value ?? '') ?>" style="<?php if ($field->type == 'number'): ?>text-align: right;<?php endif ?><?php if (in_array($field->name, $mask_fields)): ?>display: none;<?php endif ?>"><?php
-                                if ($field->type == 'icon') {
-                                    ?><i class="icon icon--gray icon--<?= @$field->translate->{$value} ?? $value ?>"></i><?php
-                                } elseif ($field->type == 'color') {
-                                    ?><span style="display: inline-block; height: 1em; width: 1em; background-color: #<?= $value ?>;">&nbsp;</span><?php
-                                } elseif ($field->type == 'file' && @$line->{"{$field->name}_path"}) {
-                                   ?><a href="/api/download/<?= $line->{"{$field->name}_path"} ?>" download><i class="icon icon--gray icon--<?= @$field->translate[$field->icon] ?? $field->icon ?>"></i></a><?php
-                                } else {
-                                    echo htmlspecialchars($value ?? '');
-                                }
-                            ?></td>
-                        <?php endforeach ?>
-                    </tr>
-                <?php endif ?>
-
-                <?php $lastgroup = @$line->date; ?>
-                <?php $seen_today = $seen_today || ($lastgroup ?? '') == $currentgroup; ?>
-            <?php endfor ?>
-        </tbody>
-    </table>
-
-    <nav>
-        <?php foreach ($addable as $linetype): ?>
-            <?php if ($linetype instanceof \ledger\linetype\Transferout): ?><?php continue; ?><?php endif ?>
-            <a href="#" class="trigger-add-line" data-type="<?= $linetype->name ?>" data-date="<?= $defaultgroup ?>"><i class="icon icon--gray icon--plus"></i> <i class="icon icon--gray icon--<?= $linetype instanceof \ledger\linetype\Transferin ? 'arrowleftright' : $linetype->icon ?>"></i></a>
-        <?php endforeach ?>
-    </nav>
-    <br><br>
-</div>
+<nav>
+    <?php foreach ($addable as $linetype): ?>
+        <?php if ($linetype instanceof \ledger\linetype\Transferout): ?><?php continue; ?><?php endif ?>
+        <a href="#" class="trigger-add-line" data-type="<?= $linetype->name ?>" data-date="<?= $defaultgroup ?>"><i class="icon icon--gray icon--plus"></i> <i class="icon icon--gray icon--<?= $linetype instanceof \ledger\linetype\Transferin ? 'arrowleftright' : $linetype->icon ?>"></i></a>
+    <?php endforeach ?>
+</nav>
+<br><br>
 
 <?php foreach ($linetypes as $linetype): ?>
     <div data-type="<?php echo $linetype->name ?>" class="line floatline edit-form" style="display: none">
