31bd9057eb6ff85978c1242763daefe1f62e118a
Oran Fry <oran@emerg.co>
2023-04-25 14:19:10 +1200

    Layouts
    Remove JSON layout in favour of subsimple bundled one.

--- classes/Router.php
+++ classes/Router.php
@@ -6,6 +6,6 @@ class Router extends \subsimple\Router
 {
     protected static $routes = [
         'GET /' => ['PAGE' => 'ledger/index'],
-        'POST /ajax/save' => ['AUTHSCHEME' => 'cookie', 'LAYOUT' => 'ledger/json', 'PAGE' => 'ledger/ajax/save'],
+        'POST /ajax/save' => ['AUTHSCHEME' => 'cookie', 'LAYOUT' => 'json', 'PAGE' => 'ledger/ajax/save'],
     ];
 }
