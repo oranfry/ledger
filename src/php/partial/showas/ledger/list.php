8f90bd91e0c7dda888a18028e55e3a9c45334a94
Oran Fry <oran@emerg.co>
2023-05-23 08:54:15 +1200

    Lists
    Remove column masking and file column type. Implement a default icon for linetypes.

--- src/php/partial/showas/ledger/list.php
+++ src/php/partial/showas/ledger/list.php
@@ -1,64 +1,52 @@
 <?php $lastgroup = 'initial'; ?>
 <?php $daterange = ContextVariableSet::get('daterange'); ?>
-<?php $num_visible_cols = count($fields) - count($mask_fields); ?>
+<?php $num_visible_cols = count($fields); ?>
 <?php $seen_today = !$has_date || !@$currentgroup || strcmp($currentgroup, $daterange->from ?? '0000-00-00') < 0 || strcmp($currentgroup, $daterange->to ?? '9999-12-31') > 0; ?>
 <table class="easy-table">
     <thead>
         <tr>
-            <th style="display: none" class="select-column printhide"><i class="icon icon--gray icon--smalldot-o selectall"></i></td></th>
             <?php foreach ($fields as $field): ?>
-                <th class="<?= $field->type == 'number' ? 'right' : '' ?>" <?php if (in_array($field->name, $mask_fields)): ?>style="display: none"<?php endif ?>><?= !@$field->supress_header && @$field->type != 'icon' ? $field->name : '' ?></th>
-            <?php endforeach ?>
+                <th class="<?= $field->type == 'number' ? 'right' : '' ?>"><?= !@$field->supress_header && @$field->type != 'icon' ? $field->name : '' ?></th>
+            <?php endforeach; ?>
         </tr>
     </thead>
     <tbody>
         <?php for ($i = 0; $i <= count($lines); $i++): ?>
-            <?php
-                $skip = false;
-                unset($line);
-
-                if ($i == count($lines)) {
-                    if (!$seen_today) {
-                        $line = (object) ['date' => $currentgroup];
-                    } else {
-                        $line = (object) ['date' => null];
-                    }
-
-                    $skip = true;
-                } else {
-                    $line = $lines[$i];
-                }
-            ?>
-
+            <?php $skip = false; ?>
+            <?php unset($line); ?>
+            <?php if ($i == count($lines)) : ?>
+                <?php if (!$seen_today) : ?>
+                    <?php $line = (object) ['date' => $currentgroup]; ?>
+                <?php else : ?>
+                    <?php $line = (object) ['date' => null]; ?>
+                <?php endif; ?>
+                <?php $skip = true; ?>
+            <?php else : ?>
+                <?php $line = $lines[$i]; ?>
+            <?php endif; ?>
             <?php if ($has_date && @$summaries[@$lastgroup] && ($i == count($lines) || @$line->date != $lastgroup)): ?>
                 <?php $summary = $summaries[$lastgroup]; ?>
                 <tr>
                     <?php foreach ($fields as $field): ?>
-                        <td class="<?= $field->type == 'number' ? 'right' : '' ?>" <?php if (in_array($field->name, $mask_fields)): ?>style="display: none"<?php endif ?>>
+                        <td class="<?= $field->type == 'number' ? 'right' : '' ?>">
                             <?php if (@$summary->{$field->name}): ?>
                                 <strong><?= @$field->prefix . $summary->{$field->name} ?></strong>
-                            <?php endif ?>
+                            <?php endif; ?>
                         </td>
-                    <?php endforeach ?>
+                    <?php endforeach; ?>
                 </tr>
-            <?php endif ?>
-
+            <?php endif; ?>
             <?php if ($i == count($lines) || @$line->date != $lastgroup): ?>
-                <?php
-                    if (!$seen_today && strcmp($currentgroup, @$line->date) < 0) {
-                        unset($line);
-
-                        $line = (object) ['date' => $currentgroup];
-                        $i--;
-                        $skip = true;
-                    }
-                ?>
-
+                <?php if (!$seen_today && strcmp($currentgroup, @$line->date) < 0) : ?>
+                    <?php unset($line); ?>
+                    <?php $line = (object) ['date' => $currentgroup]; ?>
+                    <?php $i--; ?>
+                    <?php $skip = true; ?>
+                <?php endif; ?>
                 <?php if ($i > 0): ?>
                     </tbody>
                     <tbody>
-                <?php endif ?>
-
+                <?php endif; ?>
                 <?php if (@$line->date) : ?>
                     <tr class="<?= strcmp($line->date, $currentgroup ?? '') ? '' : 'today' ?>">
                         <?php $grouphref = strtok($_SERVER['REQUEST_URI'], '?') . '?' . ($daterange ? $daterange->constructQuery(['period' => 'day', 'rawrawfrom' => $line->date]) . '&' : '') . 'back=' . base64_encode($_SERVER['REQUEST_URI']); ?>
@@ -70,20 +58,19 @@
                                     <div class="inline-modal inline-modal--right">
                                         <nav>
                                             <?php foreach ($addable as $linetype): ?>
-                                                <a href="#" class="trigger-add-line" data-type="<?= $linetype->name ?>" data-date="<?= $line->date ?>"><i class="icon icon--gray icon--<?= $linetype->icon ?>"></i></a>
-                                            <?php endforeach ?>
+                                                <a href="#" class="trigger-add-line" data-type="<?= $linetype->name ?>" data-date="<?= $line->date ?>"><i class="icon icon--gray icon--<?= $linetype->icon ?? 'doc' ?>"></i></a>
+                                            <?php endforeach; ?>
                                         </nav>
                                     </div>
                                     <a class="inline-modal-trigger"><i class="icon icon--gray icon--plus"></i></a>
                                 <?php elseif (count($addable) == 1): ?>
                                     <a href="#" class="trigger-add-line" data-type="<?= $addable[0]->name ?>" data-date="<?= $line->date ?>"><i class="icon icon--gray icon--plus"></i></a>
-                                <?php endif ?>
+                                <?php endif; ?>
                             </div>
                         </td>
                     </tr>
                 <?php endif ?>
             <?php endif ?>
-
             <?php if (!@$skip): ?>
                 <tr
                     <?= @$parent ? "data-parent=\"{$parent}\"" : '' ?>
@@ -94,72 +81,57 @@
                 >
                     <?php foreach ($fields as $fi => $field): ?>
                         <?php $value = @$field->value ? computed_field_value($line, $field->value) : @$line->{$field->name}; ?>
-                        <td data-name="<?= $field->name ?>" data-value="<?= htmlspecialchars($value ?? '') ?>" style="<?php if ($field->type == 'number'): ?>text-align: right;<?php endif ?><?php if (in_array($field->name, $mask_fields)): ?>display: none;<?php endif ?>"><?php
-                            if (!$fi) {
-                                ?><div class="select-column"><input style="display: none" type="checkbox"></div><?php
-                            }
-
-                            if ($field->type == 'icon') {
-                                ?><i class="icon icon--gray icon--<?= @$field->translate->{$value} ?? $value ?>"></i><?php
-                            } elseif ($field->type == 'color') {
-                                ?><span style="display: inline-block; height: 1em; width: 1em; background-color: #<?= $value ?>;">&nbsp;</span><?php
-                            } elseif ($field->type == 'file' && @$line->{"{$field->name}_path"}) {
-                               ?><a href="/api/download/<?= $line->{"{$field->name}_path"} ?>" download><i class="icon icon--gray icon--<?= @$field->translate[$field->icon] ?? $field->icon ?>"></i></a><?php
-                            } elseif ($field->type == 'number' && @$field->dp !== null) {
-                                echo htmlspecialchars(bcadd('0', $value ?? '0', $field->dp));
-                            } elseif ($field->type == 'number') {
-                                echo htmlspecialchars($value ?? '0');
-                            } else {
-                                echo htmlspecialchars($value ?? '');
-                            }
-                        ?></td>
-                    <?php endforeach ?>
+                        <td data-name="<?= $field->name ?>" data-value="<?= htmlspecialchars($value ?? '') ?>" style="<?= $field->type == 'number' ? 'text-align: right;' : null ?>">
+                            <?php if (!$fi) : ?><div class="select-column"><input style="display: none" type="checkbox"></div><?php endif; ?>
+                            <?php if ($field->type == 'icon') : ?>
+                                <i class="icon icon--gray icon--<?= @$field->translate->{$value} ?? $value ?>"></i>
+                            <?php elseif ($field->type == 'color') : ?>
+                                <span style="display: inline-block; height: 1em; width: 1em; background-color: #<?= $value ?>;">&nbsp;</span>
+                            <?php elseif ($field->type == 'number' && @$field->dp !== null) : ?>
+                                <?php echo htmlspecialchars(bcadd('0', $value ?? '0', $field->dp)); ?>
+                            <?php elseif ($field->type == 'number') : ?>
+                                <?php echo htmlspecialchars($value ?? '0'); ?>
+                            <?php else : ?>
+                                <?php echo htmlspecialchars($value ?? ''); ?>
+                            <?php endif; ?>
+                        </td>
+                    <?php endforeach; ?>
                 </tr>
-            <?php endif ?>
-
+            <?php endif; ?>
             <?php $lastgroup = @$line->date; ?>
             <?php $seen_today = $seen_today || ($lastgroup ?? '') == $currentgroup; ?>
-        <?php endfor ?>
+        <?php endfor; ?>
     </tbody>
 </table>
-
 <nav>
     <?php foreach ($addable as $linetype): ?>
-        <a href="#" class="trigger-add-line" data-type="<?= $linetype->name ?>" data-date="<?= $defaultgroup ?>"><i class="icon icon--gray icon--plus"></i> <i class="icon icon--gray icon--<?= $linetype instanceof \ledger\linetype\Transferin ? 'arrowleftright' : $linetype->icon ?>"></i></a>
-    <?php endforeach ?>
+        <a href="#" class="trigger-add-line" data-type="<?= $linetype->name ?>" data-date="<?= $defaultgroup ?>"><i class="icon icon--gray icon--plus"></i> <i class="icon icon--gray icon--<?= $linetype instanceof \ledger\linetype\Transferin ? 'arrowleftright' : $linetype->icon ?? 'doc' ?>"></i></a>
+    <?php endforeach; ?>
 </nav>
 <br><br>
-
 <?php foreach ($linetypes as $linetype): ?>
     <div data-type="<?php echo $linetype->name ?>" class="line floatline edit-form" style="display: none">
         <div class="lineclose">close</div>
         <h3><?= ucfirst($linetype->name) ?></h3>
         <form method="post">
-            <?php
-                foreach ($linetype->fields as $field) {
-                    if ($field->type == 'file') {
-                        $value = @$line->{"{$field->name}_path"};
-                    } else {
-                        $value = @$line->{$field->name} ?: @$_GET[$field->name] ?: @$field->default;
-                    }
-
-                    $options = @$field->options;
-
-                    if ($value && $options && !in_array($value, $options)) {
-                        array_unshift($options, $value);
-                    }
-                    ?>
-                    <div class="form-row">
-                        <div class="form-row__label"><?= @$field->label ?? $field->name ?></div>
-                        <div class="form-row__value <?= @$field->readonly ? 'noedit' : null ?>">
-                            <?php ss_require("src/php/partial/fieldtype/{$field->type}.php", compact('field', 'value', 'options')); ?>
-                        </div>
-                        <div style="clear: both"></div>
+            <?php foreach ($linetype->fields as $field) : ?>
+                <?php if ($field->type == 'file') : ?>
+                    <?php $value = @$line->{"{$field->name}_path"}; ?>
+                <?php else : ?>
+                    <?php $value = @$line->{$field->name} ?: @$_GET[$field->name] ?: @$field->default; ?>
+                <?php endif; ?>
+                <?php $options = @$field->options; ?>
+                <?php if ($value && $options && !in_array($value, $options)) : ?>
+                    <?php array_unshift($options, $value); ?>
+                <?php endif; ?>
+                <div class="form-row">
+                    <div class="form-row__label"><?= @$field->label ?? $field->name ?></div>
+                    <div class="form-row__value <?= @$field->readonly ? 'noedit' : null ?>">
+                        <?php ss_require("src/php/partial/fieldtype/{$field->type}.php", compact('field', 'value', 'options')); ?>
                     </div>
-                    <?php
-                }
-            ?>
-
+                    <div style="clear: both"></div>
+                </div>
+            <?php endforeach; ?>
             <div class="form-row">
                 <div class="form-row__label">&nbsp;</div>
                 <div class="form-row__value">
@@ -170,4 +142,4 @@
             </div>
         </form>
     </div>
-<?php endforeach ?>
+<?php endforeach; ?>
