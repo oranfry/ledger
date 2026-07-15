fd0ae71ee60100cc2eb1ff4ae12f2b2b523898e7
Oran Fry <oran@emerg.co>
2026-07-15 22:07:40 +1200

    Namespaces
    Patch a class reference.

--- classes/Router.php
+++ classes/Router.php
@@ -2,7 +2,7 @@
 
 namespace OranFry\Ledger;
 
-class Router extends OranFry\Subsimple\Router
+class Router extends \OranFry\Subsimple\Router
 {
     protected static $routes = [
         'GET /' => ['PAGE' => 'ledger/index'],
