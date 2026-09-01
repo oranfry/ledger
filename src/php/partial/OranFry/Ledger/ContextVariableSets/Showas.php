69fa60d6b8116cb012a8bde6cb082e83e2ac57d3
Oran Fry <oran@emerg.co.nz>
2026-09-01 21:37:15 +1200

    Showas
    Implement disabled state.

--- src/php/partial/OranFry/Ledger/ContextVariableSets/Showas.php
+++ src/php/partial/OranFry/Ledger/ContextVariableSets/Showas.php
@@ -1,7 +1,6 @@
 <?php
 
 ?><div class="nav-dropdown"><?php
-
     foreach ($this->options as $showas) {
         $current = $showas == $this->value ? 'current' : '';
         $icon = static::$icons[$showas];
@@ -9,13 +8,16 @@
         ?><a<?php
             ?> class="<?php
                 echo implode(' ', array_filter([
-                    'cv-manip',
+                    !$this->disabled ? 'cv-manip' : null,
                     'showas-trigger',
                     $showas == $this->value ? 'current' : null,
+                    $this->disabled ? 'disabled' : null,
                 ]));
             ?>"<?php
 
-            ?> data-manips="<?= $this->prefix ?>__value=<?= $showas?>"<?php
+            if (!$this->disabled) {
+                ?> data-manips="<?= $this->prefix ?>__value=<?= $showas?>"<?php
+            }
         ?>><?php
             ?><i class="icon icon--gray icon--<?= $icon ?>" alt="<?= $showas ?>"></i><?php
         ?></a><?php
